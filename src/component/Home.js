72942044210c88972bd6a04d45740c90697e2bb2
Bhautik Patel <bhautikpatel8494@gmail.com>
2021-11-20 16:12:16 +0530

    Revert CICD Changes

--- src/component/Home.js
+++ src/component/Home.js
@@ -40,7 +40,7 @@ function Home() {
                       <Rate allowHalf defaultValue={5} />
                       <Link to={`/${item.id}`}>
                         {' '}
-                        <button> View Product here </button>
+                        <button> View Product </button>
                       </Link>
                     </div>
                   </div>
